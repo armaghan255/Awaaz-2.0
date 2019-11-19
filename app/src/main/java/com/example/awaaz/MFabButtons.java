13c13a6764f69a8c9bfd06086203a98c5d0b3788
Ali Hamza <alihamza4772@gmail.com>
2019-11-19 23:37:56 +0500

    Initial commit

---
+++ app/src/main/java/com/example/awaaz/MFabButtons.java
@@ -0,0 +1,119 @@
+package com.example.awaaz;
+
+import android.content.Context;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ramotion.fluidslider.FluidSlider;
+
+class MFabButtons {
+    private FloatingActionButton fab_button_1, fab_button_2, fab_button_3, fab_button_4;
+    private OvershootInterpolator overshootInterpolator = new OvershootInterpolator();
+    private int fabTranslationY = 100;
+    private boolean isFabMenuOpen = false;
+    private int speed = 250;
+    private Context context;
+FluidSlider slider;
+    MFabButtons(Context context, FloatingActionButton fab_button_1, FloatingActionButton fab_button_2, FloatingActionButton fab_button_3, FloatingActionButton fab_button_4, FluidSlider slider) {
+        this.fab_button_1 = fab_button_1;
+        this.fab_button_2 = fab_button_2;
+        this.fab_button_3 = fab_button_3;
+        this.fab_button_4 = fab_button_4;
+        this.slider = slider;
+        this.context = context;
+
+       setFabTranslationY();
+      setFabClicks();
+    }
+
+    private void setFabTranslationY() {
+        fab_button_2.setAlpha(0f);
+        fab_button_3.setAlpha(0f);
+        fab_button_4.setAlpha(0f);
+
+        fab_button_2.setTranslationY(fabTranslationY);
+        fab_button_3.setTranslationY(fabTranslationY+50);
+        fab_button_4.setTranslationY(fabTranslationY+100);
+    }
+Boolean fab1,fab2,fab3,fab4=false;
+    public void setFabClicks() {
+        fab_button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuCheck();
+//                new Activity
+//                context.startActivity(new Intent(context, YourActivity));
+            }
+        });
+
+        fab_button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuCheck();
+            }
+        });
+
+        fab_button_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuCheck();
+            }
+        });
+
+        fab_button_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuCheck();
+                if (fab4==false) {
+                    slider.setVisibility(View.VISIBLE);
+               fab4=true;
+                }
+                else{
+                    slider.setVisibility(View.INVISIBLE);
+                    fab4=false;
+                }
+           }
+        });
+    }
+
+    private void fabMenuOpen() {
+        isFabMenuOpen = !isFabMenuOpen;
+        fabMainAnimation(true);
+        fabOpenAnimation(fab_button_2);
+        fabOpenAnimation(fab_button_3);
+        fabOpenAnimation(fab_button_4);
+    }
+
+    private void fabMenuClose() {
+        isFabMenuOpen = !isFabMenuOpen;
+        fabMainAnimation(false);
+        fabCloseAnimation(fab_button_2);
+        fabCloseAnimation(fab_button_3);
+        fabCloseAnimation(fab_button_4);
+    }
+
+    private void fabCloseAnimation(FloatingActionButton fab) {
+        fab.animate().translationY(fabTranslationY).alpha(0f).setInterpolator(overshootInterpolator).setDuration(speed).start();
+    }
+
+    private void fabOpenAnimation(FloatingActionButton fab) {
+        fab.animate().translationY(0f).alpha(1f).setInterpolator(overshootInterpolator).setDuration(speed+200).start();
+    }
+
+    private void fabMainAnimation(boolean isOpen) {
+        if (isOpen) {
+            fab_button_1.animate().setInterpolator(overshootInterpolator).rotation(45f).setDuration(speed).start();
+        } else {
+            fab_button_1.animate().setInterpolator(overshootInterpolator).rotation(0f).setDuration(speed).start();
+        }
+    }
+
+    private void menuCheck() {
+        if (isFabMenuOpen) {
+            fabMenuClose();
+        } else {
+            fabMenuOpen();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/awaaz/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.awaaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.camerakit.CameraKit;
+import com.camerakit.CameraKitView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ramotion.fluidslider.FluidSlider;
+
+import org.opencv.android.OpenCVLoader;
+
+import java.math.BigDecimal;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import spencerstudios.com.fab_toast.FabToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraKitView cameraKitView;
+    MFabButtons mFabButtons;
+    com.google.android.material.floatingactionbutton.FloatingActionButton fab_button_1;
+    public FluidSlider slider =null;
+
+    @SuppressWarnings("Convert2Lambda")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraKitView = findViewById(R.id.camera);
+        slider = findViewById(R.id.fluidSlider);
+        setType();
+        if(OpenCVLoader.initDebug())
+        {
+            FabToast.makeText(MainActivity.this, "OpenCV Attached Successfully", FabToast.LENGTH_LONG, FabToast.SUCCESS,  FabToast.POSITION_DEFAULT).show();
+        }
+        else
+            FabToast.makeText(MainActivity.this, "OpenCV Didn't Attached Successfully", FabToast.LENGTH_LONG, FabToast.ERROR,  FabToast.POSITION_DEFAULT).show();
+
+
+        fluidSlider();
+        slider.setVisibility(View.INVISIBLE);
+
+    }
+
+    public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+
+    void fluidSlider()
+    {
+        final float max = 7;
+        final float min = 1;
+        final float total = max - min;
+        slider.setBeginTrackingListener(new Function0<Unit>() {
+            @Override
+            public Unit invoke() {
+                return Unit.INSTANCE;
+            }
+        });
+
+        slider.setEndTrackingListener(new Function0<Unit>() {
+            @Override
+            public Unit invoke() {
+                return Unit.INSTANCE;
+            }
+        });
+
+        slider.setPositionListener(pos -> {
+            String value = String.valueOf( round((float) (min + total * pos),2) );
+            slider.setBubbleText(value);
+            return Unit.INSTANCE;
+        });
+
+        slider.setPosition(0.3f);
+        slider.setStartText(String.valueOf(min));
+        slider.setEndText(String.valueOf(max));
+    }
+
+    private void setType() {
+        fab_button_1 = findViewById(R.id.fab_button_1);
+        com.google.android.material.floatingactionbutton.FloatingActionButton fab_button_2 = findViewById(R.id.fab_button_2);
+        com.google.android.material.floatingactionbutton.FloatingActionButton fab_button_3 = findViewById(R.id.fab_button_3);
+        FloatingActionButton fab_button_4 = findViewById(R.id.fab_button_4);
+        fab_button_1.hide();
+        mFabButtons = new MFabButtons(MainActivity.this, fab_button_1, fab_button_2, fab_button_3, fab_button_4,slider);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
+        cameraKitView.setGestureListener(new CameraKitView.GestureListener() {
+            @Override
+            public void onTap(CameraKitView cameraKitView, float v, float v1) {
+                cameraKitView.setFlash(CameraKit.FLASH_ON);
+
+            }
+
+            @Override
+            public void onLongTap(CameraKitView cameraKitView, float v, float v1) {
+//     if(cameraKitView.getFlash()==CameraKit.FLASH_OFF)
+//     {
+//         cameraKitView.setFlash(CameraKit.FLASH_TORCH);
+//     }
+//     else
+//     {
+//         cameraKitView.setFlash(CameraKit.FLASH_OFF);
+//     }
+                fab_button_1.show();
+            }
+
+            @Override
+            public void onDoubleTap(CameraKitView cameraKitView, float v, float v1) {
+                if(cameraKitView.getFacing()==CameraKit.FACING_FRONT)
+                {
+                    cameraKitView.setFacing(CameraKit.FACING_BACK);
+                }
+                else
+                    cameraKitView.setFacing(CameraKit.FACING_FRONT);
+            }
+
+            @Override
+            public void onPinch(CameraKitView cameraKitView, float v, float v1, float v2) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        cameraKitView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
