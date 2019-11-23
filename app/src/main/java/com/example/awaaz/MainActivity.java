f32a997dfdb158f365c043a4d41bc3f9ddb0b221
Administrator <armghan255@gmail.com>
2019-11-23 21:32:10 +0500

    Update

--- app/src/main/java/com/example/awaaz/MainActivity.java
+++ app/src/main/java/com/example/awaaz/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.awaaz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.camerakit.CameraKit;
 import com.camerakit.CameraKitView;
@@ -11,8 +17,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ramotion.fluidslider.FluidSlider;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
+import java.lang.annotation.Annotation;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function0;
@@ -24,15 +38,17 @@ public class MainActivity extends AppCompatActivity {
     MFabButtons mFabButtons;
     com.google.android.material.floatingactionbutton.FloatingActionButton fab_button_1;
     public FluidSlider slider =null;
-
-    @SuppressWarnings("Convert2Lambda")
-    @Override
+    int sliderValue=2;
+    ImageView imageView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         cameraKitView = findViewById(R.id.camera);
+        imageView=findViewById(R.id.imgView);
         slider = findViewById(R.id.fluidSlider);
         setType();
+
         if(OpenCVLoader.initDebug())
         {
             FabToast.makeText(MainActivity.this, "OpenCV Attached Successfully", FabToast.LENGTH_LONG, FabToast.SUCCESS,  FabToast.POSITION_DEFAULT).show();
@@ -40,10 +56,27 @@ public class MainActivity extends AppCompatActivity {
         else
             FabToast.makeText(MainActivity.this, "OpenCV Didn't Attached Successfully", FabToast.LENGTH_LONG, FabToast.ERROR,  FabToast.POSITION_DEFAULT).show();
 
-
         fluidSlider();
         slider.setVisibility(View.INVISIBLE);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Tag","In Run");
+                cameraKitView.captureFrame(new CameraKitView.FrameCallback() {
+                    @Override
+                    public void onFrame(CameraKitView cameraKitView, byte[] bytes) {
+                        Toast.makeText(MainActivity.this, "Frame", Toast.LENGTH_SHORT).show();
+                        Bitmap bitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                        Mat mat=new Mat();
+                        Utils.bitmapToMat(bitmap,mat);
+                        Imgproc.cvtColor(mat,mat,Imgproc.COLOR_RGB2BGR);
+
+                    }
+                });
+                }
+
+        },2000);
     }
 
     public static float round(float d, int decimalPlace) {
@@ -51,10 +84,10 @@ public class MainActivity extends AppCompatActivity {
         bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
         return bd.floatValue();
     }
-
+    @SuppressWarnings("Convert2Lambda")
     void fluidSlider()
     {
-        final float max = 7;
+        final float max = 5;
         final float min = 1;
         final float total = max - min;
         slider.setBeginTrackingListener(new Function0<Unit>() {
@@ -74,6 +107,9 @@ public class MainActivity extends AppCompatActivity {
         slider.setPositionListener(pos -> {
             String value = String.valueOf( round((float) (min + total * pos),2) );
             slider.setBubbleText(value);
+            double temp=Double.parseDouble(value);
+            temp=temp*1000;
+            sliderValue= (int)temp;
             return Unit.INSTANCE;
         });
 
@@ -98,8 +134,25 @@ public class MainActivity extends AppCompatActivity {
         cameraKitView.setGestureListener(new CameraKitView.GestureListener() {
             @Override
             public void onTap(CameraKitView cameraKitView, float v, float v1) {
-                cameraKitView.setFlash(CameraKit.FLASH_ON);
+                Log.e("Tag","ON Tap");
+                cameraKitView.captureImage(new  CameraKitView.ImageCallback() {
+                    @Override
+                    public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
+                        // capturedImage contains the image from the CameraKitView.
+                        Toast.makeText(MainActivity.this, "Captured", Toast.LENGTH_SHORT).show();
+                        Bitmap bitmap= BitmapFactory.decodeByteArray(capturedImage,0,capturedImage.length);
+                        Mat mat=new Mat();
+                        Utils.bitmapToMat(bitmap,mat);
+                        Imgproc.cvtColor(mat,mat,Imgproc.COLOR_RGB2GRAY);
+                        Imgproc.threshold(mat,mat,0,255,Imgproc.THRESH_BINARY+Imgproc.THRESH_OTSU);
+                        List<MatOfPoint> contours = new ArrayList<>();
 
+                        Imgproc.findContours(mat,contours,mat,Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_NONE);
+                        Imgproc.drawContours(mat, contours, -1, new Scalar(0, 255, 0, 255), 3);
+                        Utils.matToBitmap(mat,bitmap);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                });
             }
 
             @Override
